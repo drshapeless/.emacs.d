8af07af49d3d19ff36f773261477ac66854a2962
Jacky Li <jacky19970420@icloud.com>
2022-02-03 16:16:32 +0800

    Update sly config for mac.

--- lisp/init-sly.el
+++ lisp/init-sly.el
@@ -7,8 +7,11 @@
 ;;; Code:
 
 (leaf sly
-  :require t
-  :config
+  :require t)
+
+;; The homebrew version of clisp is broken.
+(if *is-a-mac*
+    (setq inferior-lisp-program "sbcl")
   (setq inferior-lisp-program "clisp"))
 
 (provide 'init-sly)

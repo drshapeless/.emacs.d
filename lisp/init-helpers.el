062d4d7ea8baa2fbb89b1de18a0739cb586d5090
drshapeless <drsl@drshapeless.com>
2022-06-06 11:07:18 +0800

    Start v2ray automatically on mac.

--- lisp/init-helpers.el
+++ lisp/init-helpers.el
@@ -269,5 +269,10 @@
   (interactive)
   (drsl/switch-buffer-by-prefix "*firefox"))
 
+(if *is-a-mac*
+    (progn
+      (toggle-frame-maximized)
+      (drsl/start-v2ray)))
+
 (provide 'init-helpers)
 ;;; init-helpers.el ends here

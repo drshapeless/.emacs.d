841a9f05878a1ca1d463c767608e0bcc8e8d5ce6
drshapeless <drsl@drshapeless.com>
2022-06-12 23:17:47 +0800

    Switch from rust-mode to rustic.

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -6,15 +6,30 @@
 
 ;;; Code:
 
-(leaf rust-mode
+;; (leaf rust-mode
+;;   :require t)
+
+(leaf rustic
+  :require t
+  :mode "\\.rs\\'"
+  :config
+  (setq rustic-lsp-client 'eglot)
+  (setq rustic-format-trigger 'on-save))
+
+(leaf toml-mode
+  :mode "\\.toml\\'"
   :require t)
 
-(add-hook 'rust-mode-hook
-          (lambda () (setq indent-tabs-mode nil)))
+(leaf cargo
+  :require t
+  :hook ((rustic-mode-hook toml-mode-hook) . cargo-minor-mode))
 
-(setq rust-format-on-save t)
+;; (add-hook 'rust-mode-hook
+;;           (lambda () (setq indent-tabs-mode nil)))
 
-(define-key rust-mode-map (kbd "C-c C-c") 'rust-run)
+;; (setq rust-format-on-save t)
+
+;; (define-key rust-mode-map (kbd "C-c C-c") 'rust-run)
 
 (provide 'init-rust)
 ;;; init-rust.el ends here

ab5b9a96d7449e8e3a8db98048bd49c22febc22b
Jacky Li <drsl@drshapeless.com>
2022-06-18 09:54:06 +0800

    Use default w3m keybindings.

--- lisp/init-w3m.el
+++ lisp/init-w3m.el
@@ -13,14 +13,14 @@
   (w3m-fontify-before-hook . inherit-org-w3m-headline-fontify)
   (w3m-fontify-after-hook . inherit-org-mode)
   :bind
-  (:w3m-mode-map
-   ("l" . w3m-view-previous-page)
-   ("r" . w3m-view-next-page)
-   ("n" . w3m-next-buffer)
-   ("p" . w3m-previous-buffer)
-   ("w" . w3m-delete-buffer)
-   ("<" . w3m-tab-move-left)
-   (">" . s3m-tab-move-right))
+  ;; (:w3m-mode-map
+  ;; ("l" . w3m-view-previous-page)
+  ;; ("r" . w3m-view-next-page)
+  ;; ("n" . w3m-next-buffer)
+  ;; ("p" . w3m-previous-buffer)
+  ;; ("w" . w3m-delete-buffer)
+  ;; ("<" . w3m-tab-move-left)
+  ;; (">" . s3m-tab-move-right))
   :config
   (setq w3m-use-cookies nil
         ;; There is something encoding error when using duckduckgo.

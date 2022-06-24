429effdcaf59d7a842343e99e56f98dacf06e575
Jacky Li <drsl@drshapeless.com>
2022-06-24 22:53:34 +0800

    Revert to using shell.

--- lisp/init-shell-switcher.el
+++ lisp/init-shell-switcher.el
@@ -11,7 +11,7 @@
 
 (setq shell-switcher-mode t)
 
-;; (setq shell-switcher-new-shell-function #'shell-switcher-make-shell)
+(setq shell-switcher-new-shell-function #'shell-switcher-make-shell)
 
 (provide 'init-shell-switcher)
 ;;; init-shell-switcher.el ends here

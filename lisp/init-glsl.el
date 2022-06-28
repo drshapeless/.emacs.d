bbd2c1071a930a448cc445bd48359a8094ec7c25
Jacky Li <drsl@drshapeless.com>
2022-06-28 12:10:20 +0800

    Add .vs and .fs to glsl mode.

--- lisp/init-glsl.el
+++ lisp/init-glsl.el
@@ -9,7 +9,7 @@
 (leaf glsl-mode
   :require t
   ;; This is for bgfx shader, they are very similar.
-  :mode (("\\.sc\\'" "\\.glsl\\'" "\\.vert\\'" "\\.frag\\'" "\\.geom\\'") . glsl-mode))
+  :mode (("\\.sc\\'" "\\.glsl\\'" "\\.vert\\'" "\\.frag\\'" "\\.geom\\'" "\\.vs\\'" "\\.fs\\'") . glsl-mode))
 
 (provide 'init-glsl)
 ;;; init-glsl.el ends here

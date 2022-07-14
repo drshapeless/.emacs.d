d1add80bbf68269b88a8d1a1bbacf76e0f60c658
drshapeless <drsl@drshapeless.com>
2022-07-14 19:36:06 +0800

    Fix flutter sdk path on macos.

--- lisp/init-flutter.el
+++ lisp/init-flutter.el
@@ -11,7 +11,10 @@
 (straight-use-package 'flutter)
 (require 'flutter)
 (keymap-set dart-mode-map "C-M-x" #'flutter-run-or-hot-reload)
-(setq flutter-sdk-path "~/src/flutter/")
+(if *is-a-linux*
+    (setq flutter-sdk-path "~/src/flutter/"))
+(if *is-a-mac*
+    (setq flutter-sdk-path "/opt/homebrew/Caskroom/flutter/3.0.0/flutter"))
 
 (provide 'init-flutter)
 ;;; init-flutter.el ends here

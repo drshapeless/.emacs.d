8d681e1da65964309cef35d26ca6c9a3de3f5aaa
Jacky Li <drsl@drshapeless.com>
2022-08-09 23:17:27 +0800

    Exclude rustic mode from aggressive indent.

--- lisp/init-aggressive-indent.el
+++ lisp/init-aggressive-indent.el
@@ -10,6 +10,7 @@
 (require 'aggressive-indent)
 (global-aggressive-indent-mode 1)
 (add-to-list 'aggressive-indent-excluded-modes 'dart-mode)
+(add-to-list 'aggressive-indent-excluded-modes 'rustic-mode)
 ;; Useful in future.
 ;; (add-to-list 'aggressive-indent-excluded-modes 'html-mode)
 

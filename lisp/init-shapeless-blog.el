425656d9a8da09989ee4f57c4b4a6088b74ab84f
drshapeless <drsl@drshapeless.com>
2022-08-29 00:00:04 +0800

    Update shapeless-blog.

--- lisp/init-shapeless-blog.el
+++ lisp/init-shapeless-blog.el
@@ -8,11 +8,18 @@
 
 (straight-use-package '(shapeless-blog :type git :host github :repo "drshapeless/emacs-shapeless-blog"))
 (require 'shapeless-blog)
-(keymap-set org-mode-map "C-c b p" #'shapeless-blog-create-or-update-post)
-(keymap-set org-mode-map "C-c b t" #'shapeless-blog-update-token)
+;; This function prevent Emacs from asking password at restarting
+;; computer.
+(defun drsl/shapeless-blog-update-token ()
+  "Fetch the secret via pass before updating token."
+  (interactive)
+  (setq shapeless-blog-secret (password-store-get "blog.drshapeless.com"))
+  (shapeless-blog-update-token))
 
-(setq shapeless-blog-secret (password-store-get "blog.drshapeless.com"))
 (setq shapeless-blog-api-url "https://blog.drshapeless.com/api")
+
+(keymap-set org-mode-map "C-c b p" #'shapeless-blog-create-or-update-post)
+(keymap-set org-mode-map "C-c b t" #'drsl/shapeless-blog-update-token)
 
 (provide 'init-shapeless-blog)
 ;;; init-shapeless-blog.el ends here

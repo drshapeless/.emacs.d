1fe58fa87f6f1f77a8675f95da9624a581012678
drshapeless <drsl@drshapeless.com>
2022-09-11 11:23:01 +0800

    Remove default yasnippets dirs.

--- lisp/init-yasnippet.el
+++ lisp/init-yasnippet.el
@@ -8,7 +8,6 @@
 
 (straight-use-package 'yasnippet)
 (require 'yasnippet)
-(add-to-list 'yas-snippet-dirs "~/.emacs.d/snippets")
 (yas-global-mode t)
 (keymap-unset yas-minor-mode-map "<tab>" t)
 (keymap-set yas-minor-mode-map "C-<tab>" 'yas-expand)

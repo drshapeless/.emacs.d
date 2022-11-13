f661ba7d00f49706a155c0f73ddfca60b410313d
drshapeless <drsl@drshapeless.com>
2022-11-13 13:16:23 +0800

    Add swift-format to apheleia-formatters.

--- lisp/init-apheleia.el
+++ lisp/init-apheleia.el
@@ -8,6 +8,12 @@
 
 (straight-use-package 'apheleia)
 
+(add-to-list 'apheleia-mode-alist
+             '(swift-mode . swift-format))
+
+(add-to-list 'apheleia-formatters
+             '(swift-mode "swift-format"))
+
 (apheleia-global-mode 1)
 
 (provide 'init-apheleia)

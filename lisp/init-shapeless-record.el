f4466214767f128aaed32ea26c0e496513c394bf
drshapeless <drsl@drshapeless.com>
2023-01-22 10:18:59 +0800

    Add shapeless-record.

---
+++ lisp/init-shapeless-record.el
@@ -0,0 +1,12 @@
+;;; init-shapeless-record.el --- Record music in Emacs. -*- lexical-binding: t -*-
+
+;;; Commentary:
+
+;; I wrote this package for self use.
+
+;;; Code:
+
+(require 'shapeless-record)
+
+(provide 'init-shapeless-record)
+;;; init-shapeless-record.el ends here

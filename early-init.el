254516ff998fe055a5ee29cf3466cbd76a6b9e3f
Jacky Li <drsl@drshapeless.com>
2023-05-14 20:54:58 +0800

    Add early init.

---
+++ early-init.el
@@ -0,0 +1,12 @@
+;;; early-init.el ---  -*- lexical-binding: t -*-
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(setq package-enable-at-startup nil)
+
+(provide 'early-init)
+;;; early-init.el ends here

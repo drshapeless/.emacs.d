5948714742187b58c96359de877bf16d9b1a0e68
Jacky Li <drsl@drshapeless.com>
2023-05-23 20:13:24 +0800

    Use upstream org in straight instead of the built-in.

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -12,7 +12,8 @@
 ;; straight prevent compatibility issues.
 ;; https://github.com/raxod502/straight.el#the-wrong-version-of-my-package-was-loaded
 
-(straight-use-package '(org :type built-in))
+;; (straight-use-package '(org :type built-in))
+(straight-use-package 'org)
 (require 'org)
 (add-hook 'org-mode-hook #'auto-fill-mode)
 (add-hook 'org-mode-hook #'org-indent-mode)

7865034c75923c88e473b9d57b6c798c9d0b4638
Jacky Li <drsl@drshapeless.com>
2023-07-24 21:09:20 +0800

    Hook cape to corfu.

--- lisp/init-cape.el
+++ lisp/init-cape.el
@@ -9,7 +9,13 @@
 ;; Add extensions
 (straight-use-package 'cape)
 (require 'cape)
-(add-to-list 'completion-at-point-functions #'cape-file)
+
+(defun drsl/setup-cape ()
+  (progn
+    (add-to-list 'completion-at-point-functions #'cape-dabbrev)
+    (add-to-list 'completion-at-point-functions #'cape-file)))
+
+(add-hook 'corfu-mode-hook #'drsl/setup-cape)
 
 (provide 'init-cape)
 ;;; init-cape.el ends here

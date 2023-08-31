b55d99dfc269aa32fc9125e1342f581238cbb1f4
Jacky Li <drsl@drshapeless.com>
2023-08-31 22:57:15 +0800

    Add multiple cursors shortcut.

--- lisp/init-multiple-cursors.el
+++ lisp/init-multiple-cursors.el
@@ -9,6 +9,8 @@
 (straight-use-package 'multiple-cursors)
 (require 'multiple-cursors)
 (keymap-global-set "C-S-c C-S-c" 'mc/edit-lines)
+(keymap-global-set "C->" 'mc/mark-next-like-this)
+(keymap-global-set "C-c C->" 'mc/mark-all-like-this)
 
 (provide 'init-multiple-cursors)
 ;;; init-multiple-cursors.el ends here

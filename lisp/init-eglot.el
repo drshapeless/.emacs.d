fb98d192e06a98c3d157385d0119a12d5bab822b
Jacky Li <drsl@drshapeless.com>
2024-03-29 18:57:11 +0800

    Disable eglot-booster by default

--- lisp/init-eglot.el
+++ lisp/init-eglot.el
@@ -143,7 +143,8 @@
 ;; cd emacs-lsp-booster && cargo install --path .
 (elpaca (eglot-booster :host github :repo "jdtsmith/eglot-booster")
   (require 'eglot-booster)
-  (eglot-booster-mode))
+  ;; (eglot-booster-mode)
+  )
 
 
 (setq-default c-basic-offset 4)

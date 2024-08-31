1b8340c790a7819e3a8ff54eb4ba1606723b835c
Jacky Li <drsl@drshapeless.com>
2024-09-01 00:06:48 +0800

    Update c-arrow-mode

--- site-lisp/shapeless-c-arrow.el
+++ site-lisp/shapeless-c-arrow.el
@@ -7,16 +7,25 @@
 
 ;;; Code:
 
+(defun drsl/after-arrow ()
+  (if (looking-back "->")
+      t
+    nil))
+
 (defun shapeless-c-arrow-insert-dot ()
   (interactive)
   (let ((sp (syntax-ppss)))
-    (if (and (not (nth 4 sp))
-             (not (nth 3 sp))
-             (eq (char-before) ?\.))
-        (progn
-          (backward-delete-char 1)
-          (insert "->"))
-      (insert "."))))
+    (cond ((drsl/after-arrow)
+           (progn
+             (backward-delete-char 2)
+             (insert "...")))
+          ((and (not (nth 4 sp))
+                (not (nth 3 sp))
+                (eq (char-before) ?\.))
+           (progn
+             (backward-delete-char 1)
+             (insert "->")))
+          (t (insert ".")))))
 
 (define-minor-mode shapeless-c-arrow-mode
   "Quickly insert arrow operator. ->"

9a8d0382165dc9409a0ea1fd93751882f1d6fd11
Jacky Li <drsl@drshapeless.com>
2025-02-21 00:14:47 +0800

    Use my own odin-ts-mode

--- lisp/init-odin.el
+++ lisp/init-odin.el
@@ -6,7 +6,7 @@
 
 ;;; Code:
 
-(elpaca (odin-ts-mode :host github :repo "Sampie159/odin-ts-mode")
+(elpaca (odin-ts-mode :host github :repo "drshapeless/odin-ts-mode")
   (add-to-list 'treesit-language-source-alist
                '(odin "https://github.com/tree-sitter-grammars/tree-sitter-odin"))
   (add-to-list 'auto-mode-alist '("\\.odin\\'" . odin-ts-mode))

e6d4c922f0134f3a21632725e6b2ee6ca912f194
Jacky Li <drsl@drshapeless.com>
2025-05-07 23:51:28 +0800

    Use lsp to format glsl files

--- lisp/init-glsl.el
+++ lisp/init-glsl.el
@@ -11,7 +11,10 @@
   (require 'glsl-mode)
   (add-to-list 'auto-mode-alist '("\\.vs\\'" . glsl-mode))
   (add-to-list 'auto-mode-alist '("\\.fs\\'" . glsl-mode))
-  (add-to-list 'auto-mode-alist '("\\.comp\\'" . glsl-mode)))
+  (add-to-list 'auto-mode-alist '("\\.comp\\'" . glsl-mode))
+  (add-hook 'glsl-ts-mode-hook (lambda () (apheleia-mode 0)))
+  (add-hook 'glsl-ts-mode-hook #'eglot-format-buffer-on-save)
+  )
 
 (provide 'init-glsl)
 ;;; init-glsl.el ends here

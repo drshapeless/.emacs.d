ae1ed6e02623e53b8c56418ca031ca0051fa94b8
Jacky Li <drsl@drshapeless.com>
2025-05-30 22:32:12 +0800

    Update rust

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -7,30 +7,17 @@
 
 ;;; Code:
 
-(elpaca
-    (rustic :host github :repo "emacs-rustic/rustic")
-  (require 'rustic)
-  ;; (add-to-list 'auto-mode-alist '("\\.rs\\'" . rustic-mode))
+(use-package rust-mode
+  :ensure t
+  :init
+  (setq rust-mode-treesitter-derive t))
+
+(use-package rustic
+  :ensure t
+  :after (rust-mode)
+  :config
   (setq rustic-lsp-client 'eglot)
   (setq rustic-format-trigger 'on-save)
-  ;; This is a temporary fix for using internal treesit.
-  ;;   (if (treesit-available-p)
-  ;;       (progn
-  ;;         (define-derived-mode rustic-mode rust-ts-mode "Rustic"
-  ;;           "Major mode for Rust code.
-
-  ;; \\{rustic-mode-map}"
-  ;;           :group 'rustic
-
-  ;;           (when (bound-and-true-p rustic-cargo-auto-add-missing-dependencies)
-  ;;             (add-hook 'lsp-after-diagnostics-hook 'rustic-cargo-add-missing-dependencies-hook nil t)))
-  ;;         (require 'rust-ts-mode)
-  ;;         ))
-
-  ;;   (let ((mode '("\\.rs\\'" . rust-ts-mode)))
-  ;;     (when (member mode auto-mode-alist)
-  ;;       (setq auto-mode-alist (remove mode auto-mode-alist))))
-
   (setq rustic-analyzer-command '("rust-analyzer"))
 
   (setq rustic-ansi-faces ["#000000"
@@ -40,7 +27,9 @@
                            "#0E86D4"
                            "#FF0BAC"
                            "#5CFFFF"
-                           "#E8E8E8"]))
+                           "#E8E8E8"])
+  (add-hook 'rustic-mode-hook #'shapeless-prog-mode)
+  (add-hook 'rustic-mode-hook (lambda () (setq-local shapeless-c-arrow-p nil))))
 
 (elpaca
     toml-mode

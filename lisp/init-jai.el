ac6ee91174e7f1bac1a94c4d038efe9e0350c8ac
Jacky Li <drsl@drshapeless.com>
2025-12-31 01:14:07 +0800

    Make underscore not as Jai keyword syntax

--- lisp/init-jai.el
+++ lisp/init-jai.el
@@ -10,7 +10,8 @@
     (jai-ts-mode :host github :repo "cpoile/jai-ts-mode")
   (require 'jai-ts-mode)
   (add-to-list 'auto-mode-alist '("\\.jai\\'" . jai-ts-mode))
-  (add-hook 'jai-ts-mode-hook (lambda () (setq-local corfu-auto nil))))
+  (add-hook 'jai-ts-mode-hook (lambda () (setq-local corfu-auto nil)))
+  (modify-syntax-entry ?_ "_" jai-ts-mode--syntax-table))
 
 (provide 'init-jai)
 ;;; init-jai.el ends here

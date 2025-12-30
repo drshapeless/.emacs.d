5843d5ca82e89ed732e29a77ae5642bfd30c7344
Jacky Li <drsl@drshapeless.com>
2025-12-30 13:55:54 +0800

    Disable auto-completion in Jai

--- lisp/init-jai.el
+++ lisp/init-jai.el
@@ -9,7 +9,8 @@
 (elpaca
     (jai-ts-mode :host github :repo "cpoile/jai-ts-mode")
   (require 'jai-ts-mode)
-  (add-to-list 'auto-mode-alist '("\\.jai\\'" . jai-ts-mode)))
+  (add-to-list 'auto-mode-alist '("\\.jai\\'" . jai-ts-mode))
+  (add-hook 'jai-ts-mode-hook (lambda () (setq-local corfu-auto nil))))
 
 (provide 'init-jai)
 ;;; init-jai.el ends here

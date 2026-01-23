9c3a577e15e15329b5ac717611f1b9ccbca18a84
Jacky Li <drsl@drshapeless.com>
2026-01-23 12:22:51 +0800

    Alias jai-ts as jai

--- lisp/init-jai.el
+++ lisp/init-jai.el
@@ -11,7 +11,10 @@
   (require 'jai-ts-mode)
   (add-to-list 'auto-mode-alist '("\\.jai\\'" . jai-ts-mode))
   (add-hook 'jai-ts-mode-hook (lambda () (setq-local corfu-auto nil)))
-  (modify-syntax-entry ?_ "_" jai-ts-mode--syntax-table))
+  (modify-syntax-entry ?_ "_" jai-ts-mode--syntax-table)
+  (defalias 'jai-mode 'jai-ts-mode "For org-mode code block
+
+Otherwise, you may need to write like #+begin_src jai-ts instead of jai"))
 
 (provide 'init-jai)
 ;;; init-jai.el ends here

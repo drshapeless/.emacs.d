8481c2951833f5da6127a271ae2105d4688d1511
Jacky Li <drsl@drshapeless.com>
2026-05-01 18:16:12 +0800

    Update

--- lisp/init-swift.el
+++ lisp/init-swift.el
@@ -6,49 +6,55 @@
 
 ;;; Code:
 
-(elpaca
-    swift-ts-mode
-  (require 'swift-ts-mode)
-  (keymap-set swift-ts-mode-map "C-c p b" #'xcode-build)
-  (keymap-set swift-ts-mode-map "C-c p r" #'xcode-run)
-  (keymap-set swift-ts-mode-map "C-c p t" #'xcode-test)
-  (keymap-set swift-ts-mode-map "C-c p o" #'xcode-open-current-file))
+;; (elpaca
+;;     swift-ts-mode
+;;   (require 'swift-ts-mode)
+;;   (keymap-set swift-ts-mode-map "C-c p b" #'xcode-build)
+;;   (keymap-set swift-ts-mode-map "C-c p r" #'xcode-run)
+;;   (keymap-set swift-ts-mode-map "C-c p t" #'xcode-test)
+;;   (keymap-set swift-ts-mode-map "C-c p o" #'xcode-open-current-file))
 
-(defun print-swift-var-under-point()
-  (interactive)
-  (if (string-match-p (string (preceding-char)) "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_")
-      (backward-sexp)
-    nil)
-  (kill-sexp)
-  (yank)
-  (move-end-of-line nil)
-  (newline)
-  (insert "print(\"")
-  (yank)
-  (insert ": \\(")
-  (yank)
-  (insert ")\")")
-  (indent-for-tab-command))
+;; (defun print-swift-var-under-point()
+;;   (interactive)
+;;   (if (string-match-p (string (preceding-char)) "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_")
+;;       (backward-sexp)
+;;     nil)
+;;   (kill-sexp)
+;;   (yank)
+;;   (move-end-of-line nil)
+;;   (newline)
+;;   (insert "print(\"")
+;;   (yank)
+;;   (insert ": \\(")
+;;   (yank)
+;;   (insert ")\")")
+;;   (indent-for-tab-command))
 
-(defun xcode-build()
-  (interactive)
-  (shell-command-to-string
-   "osascript -e 'tell application \"Xcode\"' -e 'set targetProject to active workspace document' -e 'build targetProject' -e 'end tell'"))
-(defun xcode-run()
-  (interactive)
-  (shell-command-to-string
-   "osascript -e 'tell application \"Xcode\"' -e 'set targetProject to active workspace document' -e 'stop targetProject' -e 'run targetProject' -e 'end tell'"))
-(defun xcode-test()
-  (interactive)
-  (shell-command-to-string
-   "osascript -e 'tell application \"Xcode\"' -e 'set targetProject to active workspace document' -e 'stop targetProject' -e 'test targetProject' -e 'end tell'"))
+;; (defun xcode-build()
+;;   (interactive)
+;;   (shell-command-to-string
+;;    "osascript -e 'tell application \"Xcode\"' -e 'set targetProject to active workspace document' -e 'build targetProject' -e 'end tell'"))
+;; (defun xcode-run()
+;;   (interactive)
+;;   (shell-command-to-string
+;;    "osascript -e 'tell application \"Xcode\"' -e 'set targetProject to active workspace document' -e 'stop targetProject' -e 'run targetProject' -e 'end tell'"))
+;; (defun xcode-test()
+;;   (interactive)
+;;   (shell-command-to-string
+;;    "osascript -e 'tell application \"Xcode\"' -e 'set targetProject to active workspace document' -e 'stop targetProject' -e 'test targetProject' -e 'end tell'"))
 
 
-(defun xcode-open-current-file()
-  (interactive)
-  (shell-command-to-string
-   (concat "open -a \"/Applications/Xcode.app\" " (buffer-file-name))))
+;; (defun xcode-open-current-file()
+;;   (interactive)
+;;   (shell-command-to-string
+;;    (concat "open -a \"/Applications/Xcode.app\" " (buffer-file-name))))
 
+(elpaca (swift-development :host github :repo "konrad1977/swift-development"))
+
+(elpaca (swift-ts-mode :host codeberg :repo "woolsweater/swift-ts-mode")
+  (require 'swift-ts-mode))
+
+(elpaca (periphery :host github :repo "konrad1977/periphery"))
 
 (provide 'init-swift)
 ;;; init-swift.el ends here

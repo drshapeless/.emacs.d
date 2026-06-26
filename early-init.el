f8443898d00a54c4cfb95065004a3e6c44360475
Jacky Li <drsl@drshapeless.com>
2026-06-27 00:46:34 +0800

    Update homebrew gcc version

--- early-init.el
+++ early-init.el
@@ -6,6 +6,14 @@
 
 ;;; Code:
 
+(setenv "LIBRARY_PATH"
+        (mapconcat 'identity
+                   '(
+                     "/opt/homebrew/opt/gcc/lib/gcc/16"
+                     "/opt/homebrew/opt/libgccjit/lib/gcc/16"
+                     "/opt/homebrew/opt/gcc/lib/gcc/current/gcc/aarch64-apple-darwin25/16/")
+                   ":"))
+
 (setq package-enable-at-startup nil)
 
 (provide 'early-init)
